19a8b596164e786568dc27a14f4401a325674edb
T0lmIvan <tolm.vanya@gmail.com>
2024-10-08 14:04:01 +0300

    first commit

--- src/main/java/com/example/demo/Demo2Application.java
+++ src/main/java/com/example/demo/Demo2Application.java
@@ -1,13 +1,15 @@
 package com.example.demo;
 
+import com.example.demo.controller.Cont;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Demo2Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Demo2Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Demo2Application.class, args);
+        Cont con = new Cont();
+    }
 
 }
